db108ab29345982186f6b15a4134589315d93220
Hamed Salameh <hamedsalami@gmail.com>
2017-03-06 06:40:02 +0200

    Add security module

--- jsinfra.js
+++ jsinfra.js
@@ -46,22 +46,6 @@ var responseHandler = (function() {
 var generalServices = (function() {
     "use strict";
 
-    var getAntiForgeryToken = function(containerElement) {
-        var token = null;
-        // save the updates on the server
-        if (typeof containerElement !== 'undefined') {
-            var element = $('#' + containerElement);
-            if (element !== 'undefined') {
-                token = $('input[name="__RequestVerificationToken"]', element).val();
-            } else {
-                token = null;
-            }
-        } else {
-            throw new jsiException("AntiForgeryToken - Container element must not be null.", containerElement);
-        }
-        return token;
-    }
-
     var checkServerData = function(serverDataHolderId, alertBoxName, successCallbackFn) {
         try {
             var serverData = $("#" + serverDataHolderId).val();
@@ -131,8 +115,6 @@ var generalServices = (function() {
         CheckServerData: function(serverDataHolderId, alertBoxName) {
             return checkServerData(serverDataHolderId, alertBoxName);
         },
-        GetAntiForgeryToken: function(element) {
-            return getAntiForgeryToken(element);
-        }
+
     };
 })();
---
+++ modules.security.js
@@ -0,0 +1,27 @@
+var sec = (function() {
+    "use strict";
+
+    var getAntiForgeryToken = function(containerElement) {
+        var token = null;
+        // save the updates on the server
+        if (typeof containerElement !== 'undefined') {
+            var element = $('#' + containerElement);
+            if (element !== 'undefined') {
+                token = $('input[name="__RequestVerificationToken"]', element).val();
+            } else {
+                token = null;
+            }
+        } else {
+            throw new jsiException("AntiForgeryToken - Container element must not be null.", containerElement);
+        }
+        return token;
+    }
+
+    return {
+        GetAntiForgeryToken: function(containerElement) {
+            return getAntiForgeryToken(containerElement);
+        }
+    }
+
+
+})();
